f12cfcb534e8bb6dbcba6e4619a167e65acba863
pranjalmishra30 <pranjalmishra2022@gmail.com>
2020-09-30 22:43:16 +0530

    Fixed errors

--- OpenCV/pranjalmishra30/Haar_Cascade_Classifier/Haar_FaceDetection.py
+++ OpenCV/pranjalmishra30/Haar_Cascade_Classifier/Haar_FaceDetection.py
@@ -1,27 +1,21 @@
 import cv2
-import numpy as np
 
-face_cascade = cv2.CascadeClassifier('Mini-Projects/Haar_Cascade_Classifier/Data/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('Data/haarcascade_frontalface_default.xml')
 #CascadeClassifier object and file contains the face features
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0) # Video Capture object
 
 while True:
-    ret,img=cap.read()
+
+    ret,img=cap.read() # Reading each frame
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
-    faces=face_cascade.detectMultiScale(gray,1.1,6) 
-    # print(faces)
-
-    # faces=face_cascade.detectMultiScale(gray,1.9,6)
-
-    # faces=face_cascade.detectMultiScale(gray,1.1,0) 
-    # faces=face_cascade.detectMultiScale(gray,1.6,2) 
+    faces=face_cascade.detectMultiScale(gray,1.3,6) # Arguments are (Src image,Scale Factor,Min-neighbours)
     
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) # Getting the coordinates for the face rectangle
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) 
         
-    var = cv2.waitKey(1) & 0xFF    
+    var = cv2.waitKey(1) & 0xFF    # Press ESC key to exit the program
     if var == 27:
         break
 
--- OpenCV/pranjalmishra30/Haar_Cascade_Classifier/Img_FaceDetection.py
+++ OpenCV/pranjalmishra30/Haar_Cascade_Classifier/Img_FaceDetection.py
@@ -1,23 +1,15 @@
 import cv2
-import numpy
 
-img = cv2.imread('Mini-Projects/Haar_Cascade_Classifier/Data/FaceBarca.png')
-
+img = cv2.imread('Data/FaceBarca.png')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-face_cascade = cv2.CascadeClassifier('Mini-Projects/Haar_Cascade_Classifier/Data/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('Data/haarcascade_frontalface_default.xml')
 
-
-faces = face_cascade.detectMultiScale(gray,1.2,5)
-# print(faces)
-
-# faces = face_cascade.detectMultiScale(gray,1.7,5) # ScaleFactor
-
-# faces = face_cascade.detectMultiScale(gray,1.5,0) # minNeigbours
-# faces = face_cascade.detectMultiScale(gray,1.1,7) # minNeigbours
+faces = face_cascade.detectMultiScale(gray,1.2,5) # Arguments are (Src image,Scale Factor,Min-neighbours)
+# faces variable contains the coordinates of all the face-boxes detected
 
 for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2) 
 
 cv2.imshow('img',img)
 cv2.waitKey(0)
