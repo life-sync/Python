54152ce8f0afdc2ddcdbac6245a65ac366d3a0a7
Hrit20 <72162152+Hrit20@users.noreply.github.com>
2020-10-03 21:39:01 +0530

    Anagram.py

---
+++ Hrit20/Anagram.py
@@ -0,0 +1,12 @@
+def isAnagram(str1,str2):
+    return sorted(str1) == sorted(str2)
+
+
+str1 = input("Enter the string 1 : ")
+str2 = input("Enter the string 2 : ")
+
+
+if isAnagram(str1,str2):
+    print("Strings are anagram")
+else:
+    print("Strings are not anagram")
